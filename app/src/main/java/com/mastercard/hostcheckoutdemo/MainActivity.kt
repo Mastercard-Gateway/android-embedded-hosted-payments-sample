5610319ef7b2d67fa109b13097b03b64eb5eb191
Stewart Boling <e054130@mastercard.com>
2019-03-27 15:38:12 -0500

    Initial Commit

---
+++ app/src/main/java/com/mastercard/hostcheckoutdemo/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.mastercard.hostcheckoutdemo
+
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val REDIRECT_SCHEME = "simplify"
+        const val ENDPOINT_URL = "https://test-php-hosted-payment.herokuapp.com"
+        const val AMOUNT = 1000L
+        const val CURRENCY_CODE = "AUD"
+    }
+
+
+    val webView: WebView by lazy { findViewById<WebView>(R.id.webView) }
+    val checkComplete: ImageView by lazy { findViewById<ImageView>(R.id.imageView) }
+    val completeText: TextView by lazy { findViewById<TextView>(R.id.text) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.domStorageEnabled = true
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = buildWebViewClient()
+        webView.loadUrl("$ENDPOINT_URL/hostedPayment.php?amount=$AMOUNT")
+
+    }
+
+    internal fun buildWebViewClient(): WebViewClient {
+        return object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                webViewUrlChanges(Uri.parse(url))
+                return true
+            }
+        }
+    }
+
+    private fun webViewUrlChanges(uri: Uri) {
+        if (REDIRECT_SCHEME.equals(uri.scheme, ignoreCase = true)) {
+            //Parse card token out of path
+            val cardToken = uri.getQueryParameters("token")?.last()
+            processPayment(cardToken)
+
+        }
+    }
+
+    private fun processPayment(cardToken: String?) {
+        PostCardToken().execute(cardToken)
+    }
+
+
+    inner class PostCardToken : AsyncTask<String, Unit, String>() {
+        override fun doInBackground(vararg params: String?): String {
+            val url = URL("$ENDPOINT_URL/charge.php")
+            val params = "amount=$AMOUNT&simplifyToken=${params[0]}&currency=$CURRENCY_CODE"
+            val conn = url.openConnection() as HttpURLConnection
+            conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded")
+            conn.readTimeout = 15000
+            conn.connectTimeout = 15000
+            conn.requestMethod = "POST"
+            conn.doInput = true
+            conn.doOutput = true
+
+            conn.outputStream.use { os ->
+                os.bufferedWriter().use {writer ->
+                    writer.write(params)
+                }
+            }
+
+            val output = conn.inputStream.use { ins ->
+                ins.bufferedReader().use { br ->
+                    br.lineSequence().joinToString(separator = "") { it }
+                }
+            }
+
+            return output
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            Log.d("Payment Response", result)
+
+            webView.visibility = View.GONE
+            checkComplete.visibility = View.VISIBLE
+            completeText.visibility = View.VISIBLE
+        }
+    }
+}
